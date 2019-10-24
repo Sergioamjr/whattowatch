c61b726c7764a16ad1452942259f0a0c81288e38
Sergio Júnior <sergioamjr91@gmail.com>
2019-10-24 16:23:10 +0100

    Added animation in ShowItem component

--- components/ShowItem/ShowItem.js
+++ components/ShowItem/ShowItem.js
@@ -1,5 +1,6 @@
 /* eslint-disable camelcase */
 import React from "react";
+import { Transition } from "react-transition-group";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import trandingIcon from "./../../static/trending.svg";
@@ -52,20 +53,55 @@ const P = styled.p`
   padding: 0 15px;
 `;
 
+const transitionStyles = {
+  entering: { opacity: 1, transform: "translateY(0px)" },
+  entered: { opacity: 1, transform: "translateY(0px)" },
+  exiting: { opacity: 0, transform: "translateY(-30px)" },
+  exited: { opacity: 0, transform: "translateY(30px)" }
+};
+
+const duration = 500;
+
+const defaultStyle = {
+  transition: `all ${duration}ms ease`,
+  opacity: 1
+};
+
 const ShowItem = props => {
+  const [inProp, setInProp] = React.useState(false);
+  React.useEffect(() => {
+    setInProp(false);
+    setTimeout(() => {
+      setInProp(true);
+    }, 200);
+  }, []);
+
   const { title, popularity, poster_path, vote_average } = props;
   const imgUrl = poster_path
     ? `https://image.tmdb.org/t/p/w500/${poster_path}`
     : genericImg;
   return (
-    <Div className="show-item" data-test-id="test-example">
-      <img alt={`${title} poster show`} src={imgUrl} />
-      <WrapperIcons>
-        <Icon icon={trandingIcon}>{popularity}</Icon>
-        <Icon icon={rankingIcon}>{vote_average}</Icon>
-      </WrapperIcons>
-      <P>{title}</P>
-    </Div>
+    <Transition in={inProp} timeout={500}>
+      {state => {
+        return (
+          <div
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state]
+            }}
+          >
+            <Div className="show-item" data-test-id="test-example">
+              <img alt={`${title} poster show`} src={imgUrl} />
+              <WrapperIcons>
+                <Icon icon={trandingIcon}>{popularity}</Icon>
+                <Icon icon={rankingIcon}>{vote_average}</Icon>
+              </WrapperIcons>
+              <P>{title}</P>
+            </Div>
+          </div>
+        );
+      }}
+    </Transition>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,11 @@
 import React from "react";
-import { Transition } from "react-transition-group";
+
 import Layout from "../components/Layout/Layout";
 import styled from "styled-components";
 import GenrerList from "../components/GenrerList/GenrerList";
 import { AppContext } from "./_app";
 import { fetchMovies } from "../services";
 import DynamicInfiniteScroll from "../components/InfiniteScroll";
-
-const duration = 500;
-
-const defaultStyle = {
-  transition: `all ${duration}ms ease`,
-  opacity: 1
-};
-
-const transitionStyles = {
-  entering: { opacity: 1, transform: "translateX(0px)" },
-  entered: { opacity: 1, transform: "translateX(0px)" },
-  exiting: { opacity: 0, transform: "translateX(-30px)" },
-  exited: { opacity: 0, transform: "translateX(30px)" }
-};
 
 const H1 = styled.h1`
   margin-bottom: 30px;
@@ -29,10 +15,8 @@ const Home = () => {
   const [page, setPage] = React.useState(1);
   const { genres } = React.useContext(AppContext);
   const [movies_, setMovies_] = React.useState([]);
-  // const { inProp } = fetchAllMovies(page);
 
   const fetchMore = async f => {
-    console.log("fetch");
     try {
       const newMovies = await fetchMovies(page);
       setMovies_(movies_.concat(newMovies));
@@ -42,40 +26,27 @@ const Home = () => {
 
   return (
     <Layout>
-      <Transition in={true} timeout={500}>
-        {state => {
-          return (
-            <div
-              style={{
-                ...defaultStyle,
-                ...transitionStyles[state]
-              }}
-            >
-              <div>
-                <H1 className="title" data-testid="title">
-                  Lasts movies
-                </H1>
-                <GenrerList genres={genres} />
-                <DynamicInfiniteScroll
-                  config={{
-                    loadMore: fetchMore,
-                    hasMore: page < 499
-                  }}
-                  items={movies_.sort((a, b) => {
-                    if (a.popularity > b.popularity) {
-                      return -1;
-                    }
-                    if (a.popularity < b.popularity) {
-                      return 1;
-                    }
-                    return 0;
-                  })}
-                />
-              </div>
-            </div>
-          );
-        }}
-      </Transition>
+      <div>
+        <H1 className="title" data-testid="title">
+          Lasts movies
+        </H1>
+        <GenrerList genres={genres} />
+        <DynamicInfiniteScroll
+          config={{
+            loadMore: fetchMore,
+            hasMore: page < 499
+          }}
+          items={movies_.sort((a, b) => {
+            if (a.popularity > b.popularity) {
+              return -1;
+            }
+            if (a.popularity < b.popularity) {
+              return 1;
+            }
+            return 0;
+          })}
+        />
+      </div>
     </Layout>
   );
 };
