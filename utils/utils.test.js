0c46ffe5c4b10e1a1488704592dcf2fe20f14caf
Sergio Júnior <sergioamjr91@gmail.com>
2019-11-02 19:17:01 +0000

    Added now json

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -3,6 +3,6 @@ import { generateRandonNumber } from "./";
 describe("Util methods", () => {
   test("should return a randon number between a range", () => {
     const n = generateRandonNumber(0, 10);
-    expect(n > 0 && n < 10).toBe(true);
+    expect(n > 0 && n <= 10).toBe(true);
   });
 });
